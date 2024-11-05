b529890b3de8ac4a9000604652dbf4842679f264
anantha3267 <ananthamchiranjeevi@gmail.com>
2024-11-05 09:08:28 -0500

    python terraform project

--- Terraform/Terraform_Projects/terraform-deploy-python-app/terraform-files/userdata.sh
+++ Terraform/Terraform_Projects/terraform-deploy-python-app/terraform-files/userdata.sh
@@ -5,7 +5,17 @@ apt update
 apt install docker.io -y
 
 #create new directory
-mkdir /home/ubuntu/app
+mkdir /home/ubuntu/gitclone
+mkdir /home/ubuntu/pythonapp
 
-cd /home/ubuntu/app
+cd /home/ubuntu/gitclone
 
+git clone https://github.com/anantha3267/Projects.git
+
+cp -r Terraform_Projects/terraform-deploy-python-app/* /home/ubuntu/pythonapp
+
+cd /home/ubuntu/pythonapp
+
+docker build -t myflaskappv1 .
+
+docker run -p 5000:5000 myflaskappv1
