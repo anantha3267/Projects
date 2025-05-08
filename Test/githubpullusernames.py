c36cf2cd0ff6eca24c8a3030ca21c07ab1ac580b
anantha3267 <ananthamchiranjeevi@gmail.com>
2025-05-08 04:16:27 -0400

    removed Test

--- Test/githubpullusernames.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-data = requests.get("https://api.github.com/repos/kubernetes/kubernetes/pulls")
-
-full_data = data.json()
-print(full_data[1]["user"]["login"])
-
-for i in full_data:
-    print(i["user"]["login"])
--- Test/read_data.py
+++
@@ -1,6 +0,0 @@
-with open(r"C:\Users\nikhi\OneDrive\Desktop\FT\Projects\Test\logs.log","r") as file:
-    data = file.readlines()
-    print(data)
-
-for line in data:
-    print(line)
